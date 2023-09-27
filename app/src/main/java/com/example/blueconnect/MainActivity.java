a89daffbc3fa3b0450e9139f99a78b2bfb94e5c5
nscressy <nscressy@csuchico.edu>
2023-09-26 19:27:43 -0700

    Bluetooth enable is working, disable is working (remove disable completely?), list paired devices working, scan for devices functionality all commented out scan button is visible but doesn't do anything.

--- app/src/main/java/com/example/blueconnect/MainActivity.java
+++ app/src/main/java/com/example/blueconnect/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -13,51 +17,115 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button blueOn, blueOff, showDeviceList;
-    ListView listView;
+    Button blueOn, blueOff, showDeviceList;//, scanDevices;
+    ListView listView;//, scanListView;
     BluetoothAdapter blueAdapter;
-
-    Intent btEnable;
+    //ArrayList<String> stringArrayList=new ArrayList<String>();
+    //ArrayAdapter<String> arrayAdapter;
+    Intent btEnable, btDisable;
     int requestEnableCode;
+    private ActivityResultLauncher<Intent> enablebt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        blueOn=(Button) findViewById(R.id.btOn);
-        blueOff=(Button) findViewById(R.id.btOff);
-        showDeviceList=(Button) findViewById(R.id.ShowDevices);
-        blueAdapter=BluetoothAdapter.getDefaultAdapter();
-        if(blueAdapter == null)
-        {
+        blueOn = (Button) findViewById(R.id.btOn);
+        blueOff = (Button) findViewById(R.id.btOff);
+        showDeviceList = (Button) findViewById(R.id.ShowDevices);
+//        scanDevices=(Button) findViewById(R.id.findDevices);
+        blueAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (blueAdapter == null) {
             Toast.makeText(getApplicationContext(), "Bluetooth not supported on this device", Toast.LENGTH_LONG).show();
-            return;
         }
-        btEnable=new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        requestEnableCode=1;
-        listView=(ListView) findViewById(R.id.listview);
+        btEnable = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        requestEnableCode = 1;
+        listView = (ListView) findViewById(R.id.listview);
+//        scanListView=(ListView) findViewById(R.id.scannedListView);
 
+        enablebt = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "Bluetooth Enabled", Toast.LENGTH_LONG).show();
+            } else if (result.getResultCode() == RESULT_CANCELED) {
+                Toast.makeText(getApplicationContext(), "Bluetooth Enabling Cancelled", Toast.LENGTH_LONG).show();
+            }
+        });
+
+/*        scanDevices.setOnClickListener(new View.OnClickListener() {
+            //@SuppressLint("MissingPermission")
+            @Override
+            public void onClick (View v) {
+                try {
+                    blueAdapter.startDiscovery();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(myReceiver, intentFilter);
+
+        arrayAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,stringArrayList);
+        scanListView.setAdapter(arrayAdapter);
+*/
         bluetoothOn();
         bluetoothOff();
         showListOfDevices();
+
     }
 
-        private void bluetoothOff() {
-        blueOff.setOnClickListener((new View.OnClickListener() {
-            @SuppressLint("MissingPermission")
+    private void bluetoothOn() {
+        blueOn.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(blueAdapter == null)
-                {
+                if (blueAdapter == null) {
+                    Toast.makeText(getApplicationContext(), "This device does not support Bluetooth", Toast.LENGTH_LONG).show();
+                } else {
+                    if (!blueAdapter.isEnabled()) {
+                        enablebt.launch(btEnable);
+                    }
+                }
+            }
+        }));
+    }
+
+    /*   BroadcastReceiver myReceiver=new BroadcastReceiver() {
+           //@SuppressLint("MissingPermission")
+           @Override
+           public void onReceive(Context context, Intent intent) {
+               String action = intent.getAction();
+                   if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                       BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                       stringArrayList.add(device.getName());
+                       arrayAdapter.notifyDataSetChanged();
+                   }
+           }
+       };
+   */
+    private void bluetoothOff() {
+        blueOff.setOnClickListener((new View.OnClickListener() {
+            //@SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View v) {
+                if (blueAdapter == null) {
                     Toast.makeText(getApplicationContext(), "Bluetooth not supported on this device", Toast.LENGTH_LONG).show();
                     return;
                 }
                 try {
                     if (blueAdapter != null && blueAdapter.isEnabled()) {
+                        Toast.makeText(getApplicationContext(), "Bluetooth Disabled", Toast.LENGTH_LONG).show();
                         blueAdapter.disable();
                     }
                 }
@@ -69,39 +137,9 @@ public class MainActivity extends AppCompatActivity {
         }));
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == requestEnableCode) {
-            if (resultCode == RESULT_OK) {
-                Toast.makeText(getApplicationContext(), "Bluetooth Enabled", Toast.LENGTH_LONG).show();
-            } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(getApplicationContext(), "Bluetooth Enabling Cancelled", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-    private void bluetoothOn() {
-        blueOn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(blueAdapter==null)
-                {
-                    Toast.makeText(getApplicationContext(), "This device does not support Bluetooth", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                else
-                {
-                    if(!blueAdapter.isEnabled())
-                    {
-                        startActivityForResult(btEnable, requestEnableCode);
-                    }
-                }
-            }
-        });
-    }
-
     private void showListOfDevices() {
         showDeviceList.setOnClickListener((new View.OnClickListener() {
-            @SuppressLint("MissingPermission")
+            //@SuppressLint("MissingPermission")
             @Override
             public void onClick(View v) {
                 if(blueAdapter == null)
@@ -110,7 +148,8 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
                 try {
-                    @SuppressLint("MissingPermission") Set<BluetoothDevice> btset = blueAdapter.getBondedDevices();
+                    //@SuppressLint("MissingPermission")
+                    Set<BluetoothDevice> btset = blueAdapter.getBondedDevices();
 
                     String[] deviceList = new String[btset.size()];
                     int i = 0;
